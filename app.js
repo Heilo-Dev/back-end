79a1e72f5b48fad576d4207fed37ce15f82a5b62
mehedi494 <ehasanmunna21@gmail.com>
2022-11-28 18:49:32 +0600

    aggregate mixin

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use("/api/v1/student", studentRouter)
 
 
 app.use("/*", async (req, res) => {
+    console.log(req);
     res.status(404).json({
         status: "fail",
         message:"No routes found"
