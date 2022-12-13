d39130f4b46e97baef88076ed38bc8e868ecc1ce
mehedi494 <ehasanmunna21@gmail.com>
2022-12-13 10:06:02 +0600

    admin wallate request get api

--- controller/admin.controller.js
+++ controller/admin.controller.js
@@ -11,8 +11,32 @@ exports.homeGet = async (req, res, next) => {
             })
         }
 
-        const result = await adminServices.homeGetService()
-console.log( result);
+exports.adminWallateController = async (req, res, next) => {
+    try {
+     
+        const result = await adminServices.adminWallateService()
+ 
+    //    console.log(result);
+        res.status(200).json({
+            status: "success",
+            result
+        })
+
+    } catch (error) {
+        // console.log(error);
+        res.status(400).json({
+            status: "fail",
+            error
+        })
+
+    }
+}
+exports.purchaseReqController = async (req, res, next) => {
+    try {
+     
+        const updateData = await adminServices.purchaseReqService(req.body)
+ 
+    //    console.log(result);
         res.status(200).json({
             status: "success",
             result
