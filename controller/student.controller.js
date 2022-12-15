ee138a3656cf53d3962a8578dce828ae6eec23a8
Lil-boss <rshaon09@gmail.com>
2022-12-15 17:57:55 +0600

    updated

--- controller/student.controller.js
+++ controller/student.controller.js
@@ -1,86 +1,74 @@
-const studentServices = require("../services/student.services")
-
+const studentServices = require("../services/student.services");
 
 exports.ondemand = async (req, res, next) => {
-    try {
+  try {
+    const { gender, range, subject, availability, topic, limit, id } =
+      req.query;
 
+    let filter = {};
 
-
-        const { gender, range, subject, availability, topic, limit, id } = req.query;
-
-        let filter = {}
-
-        if (gender) {
-            filter.gender = gender
-
-        }
-        if (range) {
-            filter.hourlyRate = {
-                "$lte": (+req.query.range)
-            }
-
-
-        }
-        if (subject) {
-            filter["tuitionSubjects.name"] = subject
-        }
-
-        if (availability) {
-            filter.availability = availability;
-        }
-
-        const result = await studentServices.getAllByFilter(filter)
-        // console.log(result);
-
-        if (result.result.length == 0) {
-            return res.status(200).json({
-                status: "Not found"
-            })
-        }
-        
-        res.status(200).json({
-            status: "success",
-            found: result.count[0].found,
-            result: result.result
-        })
-
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({
-            status: "fail",
-            error
-        })
-
+    if (gender) {
+      filter.gender = gender;
     }
-}
+    if (range) {
+      filter.hourlyRate = {
+        $lte: +req.query.range,
+      };
+    }
+    if (subject) {
+      filter["tuitionSubjects.name"] = subject;
+    }
 
+    if (availability) {
+      filter.availability = availability;
+    }
 
+    const result = await studentServices.getAllByFilter(filter);
+    // console.log(result);
+
+    if (result.result.length == 0) {
+      return res.status(200).json({
+        status: "Not found",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      found: result.count[0].found,
+      result: result.result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
 
 exports.updaterStudentProfile = async (req, res, next) => {
-    try {
-        const { email } = req.user
-        const user = await studentServices.getStudentFindByEmail(email)
-        console.log("user ", user);
-        if (user.role != "student") {
-            return res.status(401).json({
-                status: "fail",
-                error: "not authorized for this route"
-            })
-        }
-
-        const result = await studentServices.updaterStudentProfile(email, req.body)
-
-        res.status(200).json({
-            status: "success",
-            result
-        })
-
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({
-            status: "fail",
-            error
-        })
-
+  try {
+    const { email } = req.user;
+    const user = await studentServices.getStudentFindByEmail(email);
+    console.log("user ", user);
+    if (user.role != "student") {
+      return res.status(401).json({
+        status: "fail",
+        error: "not authorized for this route",
+      });
     }
-}
+
+    const result = await studentServices.updaterStudentProfile(email, req.body);
+
+    res.status(200).json({
+      status: "success",
+      result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
--- routes/v1/student.route.js
+++ routes/v1/student.route.js
@@ -1,10 +1,9 @@
-const express = require('express');
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const userController = require("../../controller/student.controller");
-const verifyToken = require('../../middleware/verifyToken');
+const verifyToken = require("../../middleware/verifyToken");
 
-
-router.get("/ondemand", userController.ondemand)
-router.patch("/update",verifyToken, userController.updaterStudentProfile)
+router.get("/ondemand", userController.ondemand);
+router.patch("/update", verifyToken, userController.updaterStudentProfile);
 
 module.exports = router;
