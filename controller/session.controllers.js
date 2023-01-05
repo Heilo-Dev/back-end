a01d6b9b6ae1257c44441a58420755e41a2a997a
Lil-boss <rshaon09@gmail.com>
2023-01-05 18:28:43 +0600

    new item added

--- controller/session.controllers.js
+++ controller/session.controllers.js
@@ -3,6 +3,8 @@ const {
   generateSessionLink,
   confirmSession,
   decrementBalance,
+  allEvents,
+  tempDb,
 } = require("../services/session.service");
 
 //create session req from student
@@ -40,31 +42,58 @@ exports.confirmSession = async (req, res) => {
   try {
     const id = req.body.id; //session id
     const status = req.body.status;
+    //confirm session
     await confirmSession(id, status);
+
     if (status === "accept") {
       /**before the generate link access the wallet and decrement the money by teacher hour rate
        * and the hold it another DB
        */
 
       //decrement the balance form student wallet
-      await decrementBalance(id);
-
+      const result = await decrementBalance(id);
+      console.log(result);
       //link generate
-      const eventData = (data) => {
-        console.log(data);
-        return data;
+      const eventData = async (data) => {
+        //store the sessionID, order_money, generated link in tempDB;
+
+        const tempData = {
+          sessionId: id,
+          generated_link: data.htmlLink,
+        };
+
+        const result = await tempDb(tempData);
+        res.status(200).json({
+          status: "success",
+          message: "Request update Successfully",
+          data: { data, result },
+        });
       };
       await generateSessionLink(id, eventData);
-
-      res.status(200).json({
-        status: "success",
-        message: "Request update Successfully",
-      });
     }
   } catch (error) {
     res.status(400).json({
       status: "failed",
       message: "Request failed Successfully",
+      error: error.message,
+    });
+  }
+};
+
+exports.getTheEvents = async (req, res) => {
+  try {
+    const getAllEvents = (data) => {
+      res.status(400).json({
+        status: "success",
+        message: "Request success",
+        data: data,
+      });
+    };
+    await allEvents(getAllEvents);
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      message: "Request failed",
       error: error.message,
     });
   }
