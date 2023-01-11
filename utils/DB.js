3e9db958abc5a3a5fabb26e584c3d79ba946c2fa
mehedi494 <ehasanmunna21@gmail.com>
2023-01-11 23:58:02 +0600

    changing console color

--- utils/DB.js
+++ utils/DB.js
@@ -9,7 +9,7 @@ module.exports = database = () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
-        .then(() => console.log(`Database Connected successfull, ${text.toLocaleString()}`.bgGreen.black))
+        .then(() => console.log(`Database Connected successfull, ${text.toLocaleString()}`.bgGreen.white))
         .catch("Connect to fail....");
 };
 
